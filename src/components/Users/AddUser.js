d876729be30de71bc16c435e96e655d9c6a18709
mojahid-belaman <mojahidbelaman@gmail.com>
2023-05-20 17:05:43 +0100

    modify in style Card

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,16 +1,20 @@
 import React from "react";
+import Card from "../UI/Card";
 
 function AddUser() {
   const addSubmit = (e) => {
     e.preventDefault();
   };
   return (
-    <form onSubmit={addSubmit}>
-      <label htmlFor="username">Username</label>
-      <input name="username" type="text" />
-      <label htmlFor="age">Age (Years)</label>
-      <input name="age" type="number" />
-    </form>
+    <Card className={'conatiner mx-auto my-8 p-4 w-[90%] max-w-2xl'}>
+      <form onSubmit={addSubmit}>
+        <label htmlFor="username" className="block font-bold mb-2">Username</label>
+        <input name="username" type="text" className="block mb-2 w-full rounded-sm border focus:border-0 border-gray-300 p-1 outline-none focus:outline-gray-500" />
+        <label htmlFor="age" className="block font-bold mb-2">Age (Years)</label>
+        <input name="age" type="number" className="block mb-2 w-full rounded-sm border focus:border-0 border-gray-300 p-1 outline-none focus:outline-gray-500 " />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 }
 
