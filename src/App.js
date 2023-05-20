75a4795b89223433f05eab79d90346350b3a7b28
mojahid-belaman <mojahidbelaman@gmail.com>
2023-05-20 16:12:20 +0100

    integrate AddUser In App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import AddUser from "./components/Users/AddUser";
 import "./index.css";
 
 const App = () => {
-  return <div className="h-screen py-10 bg-[#212121]"></div>;
+  return (
+    <div className="h-screen py-10 bg-[#212121]">
+      <AddUser />
+    </div>
+  );
 };
 
 export default App;
