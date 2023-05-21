e524068e28832beee54efe0eed6e0e2420219bfb
mojahid-belaman <mojahidbelaman@gmail.com>
2023-05-21 13:30:35 +0100

    add state error to handle input name and age

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,57 +1,73 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
-function AddUser() {
+const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
-  
+  const [error, setError] = useState();
+
+  const clickHandler = () => {
+    setError();
+  };
   const changeUsernameHandler = (e) => {
     setEnteredUsername(e.target.value);
-  }
+  };
   const changeAgeHandler = (e) => {
     setEnteredAge(e.target.value);
-  }
+  };
 
   const addSubmit = (e) => {
     e.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
-        return ;
+      setError({
+        title: "Invalid Input",
+        message: "Please enter a valid username and age (non-empty values).",
+      });
+      return;
     }
     if (+enteredAge < 0) {
-        return ;
+      setError({
+        title: "Invalid Age",
+        message: "Please enter a valid age ( > 0).",
+      });
+      return;
     }
-    console.log(enteredUsername, enteredAge);
-    setEnteredUsername('');
-    setEnteredAge('');
+    props.onAddUser(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
   };
   return (
-    <Card className={"conatiner mx-auto my-8 p-4 w-[90%] max-w-2xl"}>
-      <form onSubmit={addSubmit}>
-        <label htmlFor="username" className="block font-bold mb-2">
-          Username
-        </label>
-        <input
-          name="username"
-          type="text"
-          className="block mb-2 w-full rounded-sm border focus:border-0 border-gray-300 p-1 outline-none focus:outline-gray-500"
-          value={enteredUsername}
-          onChange={changeUsernameHandler}
-        />
-        <label htmlFor="age" className="block font-bold mb-2">
-          Age (Years)
-        </label>
-        <input
-          name="age"
-          type="number"
-          className="block mb-2 w-full rounded-sm border focus:border-0 border-gray-300 p-1 outline-none focus:outline-gray-500 "
-          value={enteredAge}
-          onChange={changeAgeHandler}
-        />
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+    <div>
+      {error && <ErrorModal error={error} onHideError={clickHandler} />}
+      <Card className={"conatiner mx-auto my-8 p-4 w-[90%] max-w-2xl"}>
+        <form onSubmit={addSubmit}>
+          <label htmlFor="username" className="block font-bold mb-2">
+            Username
+          </label>
+          <input
+            name="username"
+            type="text"
+            className="block mb-2 w-full rounded-sm border focus:border-0 border-gray-300 p-1 outline-none focus:outline-gray-500"
+            value={enteredUsername}
+            onChange={changeUsernameHandler}
+          />
+          <label htmlFor="age" className="block font-bold mb-2">
+            Age (Years)
+          </label>
+          <input
+            name="age"
+            type="number"
+            className="block mb-2 w-full rounded-sm border focus:border-0 border-gray-300 p-1 outline-none focus:outline-gray-500 "
+            value={enteredAge}
+            onChange={changeAgeHandler}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
-}
+};
 
 export default AddUser;
