b922aa5d38fa7a7369031d9918fecb2319941203
mojahid-belaman <mojahidbelaman@gmail.com>
2023-05-21 13:32:09 +0100

    add component UserList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 import "./index.css";
 
 const App = () => {
+  const [dataUsers, setDataUsers] = useState([]);
+  const addUser = (uName, uAge) => {
+    setDataUsers((prev) => {
+      return [...prev, { id: Math.random(), name: uName, age: uAge }];
+    });
+  };
   return (
     <div className="h-screen py-10 bg-[#212121]">
-      <AddUser />
+      <AddUser onAddUser={addUser} />
+      <UsersList users={dataUsers} />
     </div>
   );
 };
